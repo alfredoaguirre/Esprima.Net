96e7f80a65db52e1ec941e14ddfd7692c3f48aa7
Daniel Evilsizor <daniel@homekeystudios.com>
2017-02-12 19:34:50 -0600

    Added jquery unit test

---
+++ JavaScriptToNet.Test/jQuery/jQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JavaScriptToNet.Test
+{
+    [TestClass]
+    public class jQuery
+    {
+        [TestMethod]
+        public void core()
+        {
+			//todo
+        }
+
+    }
+}
