5913d5e0047539a0aa961955f13f42b76c52494d
Daniel Evilsizor <daniel@homekeystudios.com>
2017-02-12 20:42:55 -0600

    update file encoding

--- JavaScriptToNet.Test/jQuery/jsonStructure.cs
+++ JavaScriptToNet.Test/jQuery/jsonStructure.cs
@@ -2,23 +2,23 @@ using System;
 
 namespace JavaScriptToNet.Test
 {
-	public class jsonAPIObject
-	{
-		public string name;
-		public string type;
-		public String[] args;
-		public String[] properties;
-		public jsonAPIMethod[] methods;
-	}
+    public class jsonAPIObject
+    {
+        public string name;
+        public string type;
+        public String[] args;
+        public String[] properties;
+        public jsonAPIMethod[] methods;
+    }
     
-	public class jsonAPIMethod 
-	{
-		public string name;
-		public String[] args;
-	}
-	
+    public class jsonAPIMethod 
+    {
+        public string name;
+        public String[] args;
+    }
+    
     public class jsonStructure
     {
-		public jsonAPIObject[] apiNames;
-	}
+        public jsonAPIObject[] apiNames;
+    }
 }
